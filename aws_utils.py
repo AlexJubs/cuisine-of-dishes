baa5e89c54ffdbdfe46045f4d85a079dc897e174
Alexander Jabbour <ajabbour@uwaterloo.ca>
2020-10-22 00:37:39 -0400

    add dish works now

--- aws_utils.py
+++ aws_utils.py
@@ -39,11 +39,32 @@ class aws_utils:
         return "Created table in DynamoDB for your dishes!"
 
     def get_all_dishes():
-        return "ok"
+        return_message = ''
+        # make sure that a table exists
+        if TableName not in dynamodb_client.list_tables()["TableNames"]:
+            return_message += "Database table for dishes does not exist, creating one \n"
+            aws_utils.assert_table_exists()
+
+        # return a list of all table items
+        return return_message + str(dynamodb_client.scan(TableName=TableName)["Items"])
 
     def add_dish(dish_name):
         # check if dish exists if no, return "dish already exists". if yes, return "removed <dish>"
-        return "ok"
+        return_message = ''
+        # make sure that a table exists
+        if TableName not in dynamodb_client.list_tables()["TableNames"]:
+            return_message += "Database table for dishes does not exist, creating one \n"
+            aws_utils.assert_table_exists()
+
+        # add / update the item in dynamodb
+        dynamodb_client.update_item(
+            TableName=TableName, 
+            Key= {
+                'Dish': {"S": dish_name}
+                }
+            )
+
+        return return_message + "Added dish in the database!"
 
     def remove_dish(dish_name):
         # check is dish exists. if no, return error. if yes, return "removed <dish>"
